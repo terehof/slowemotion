8936227167b7d6cc75088358516ed4056359c380
terehov <terehovsergej@gmail.com>
2015-12-05 00:45:40 +0200

    final changes!

--- src/send.php
+++ src/send.php
@@ -1,7 +1,7 @@
 <?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
-    $to = 'terehovsergej@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
-    $subject = 'Обратный звонок'; //Загаловок сообщения
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ 
+    $to = 'koveshnikoff@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+    $subject = 'lowemotion.com.ua - обратный звонок'; //Загаловок сообщения
     $message = '
                 <html>
                     <head>
@@ -14,6 +14,6 @@ if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST[
                 </html>'; //Текст нащего сообщения можно использовать HTML теги
     $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
     $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
-    mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+    mail($to, $subject, $message, $headers);
 }
 ?>
